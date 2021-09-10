01af9354981fd14b1ff9c642f764ab0eeccecc91
Dinesh <d2552002@gmail.com>
2021-09-10 15:34:19 +0530

    minor

--- script.js
+++ script.js
@@ -6,12 +6,12 @@ var scroll = window.requestAnimationFrame ||
                 elementsToShow.forEach(function (element) {
                   if (isElementInViewport(element)) {
                     element.classList.add('active');
-                    
+
                   } else {
                     element.classList.remove('active');
                   }
                 });
-              
+
                 scroll(loop);
               }
               loop();
@@ -32,3 +32,30 @@ var scroll = window.requestAnimationFrame ||
                     rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
                 );
               }
+
+function madras(){
+
+  var madras=document.querySelector(".madras");
+  madras.style.display="block";
+  var btn=document.querySelector(".intro-text").style;
+  btn.color="black";
+  var ch=document.querySelector(".chennai");
+  if(ch.style.display != "none"){
+    ch.style.display="none"; 
+  }
+  var btn1=document.querySelector(".intro-text1").style;
+  btn1.color="white";
+}
+function chennai(){
+  
+  var chennai=document.querySelector(".chennai");
+  chennai.style.display="block";
+  var btn=document.querySelector(".intro-text1").style;
+  btn.color="red";
+  var ch=document.querySelector(".madras");
+  if(ch.style.display != "none"){
+    ch.style.display="none";
+  }
+   var btn1=document.querySelector(".intro-text").style;
+  btn1.color="white";
+}
